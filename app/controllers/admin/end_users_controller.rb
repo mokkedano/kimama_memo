a057c2c51600d515aa11641774ba7c4cf8c7f71b
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-05-13 13:15:35 +0000

    [Add] 管理者側ユーザー編集画面

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::EndUsersController < ApplicationController
+  before_action :set_end_user, only: [:show, :edit, :update]
 
 
   def index
@@ -7,7 +8,20 @@ class Admin::EndUsersController < ApplicationController
 
 
   def show
-    @end_user = EndUser.find(params[:id])
+  end
+
+
+  def edit
+  end
+
+
+  def update
+    # binding.pry
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path(@end_user)
+    else
+      render :edit
+    end
   end
 
 
@@ -15,7 +29,12 @@ class Admin::EndUsersController < ApplicationController
 
   private
   def end_user_params
-    params.require(:end_user).permit(:nickname, :email, :self_introduction, :is_deleted)
+    params.require(:end_user).permit(:nickname, :email, :self_introduction, :is_deleted, :profile_image)
+  end
+
+
+  def set_end_user
+    @end_user = EndUser.find(params[:id])
   end
 
 
