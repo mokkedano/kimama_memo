8abe0d4db7fa8545c8e6e1a8c6817757b7155786
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-05-17 07:31:43 +0000

    [Add] グループ機能

--- app/controllers/public/chat_messages_controller.rb
+++ app/controllers/public/chat_messages_controller.rb
@@ -3,12 +3,17 @@ class Public::ChatMessagesController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
+    # @messages = Group.find(params[:group_id]).chat_message
+    @message = ChatMessage.new
+    @messages = ChatMessage.where(group: @group)
   end
 
 
   def create
-    @messages = ChatMessage.new(message_params)
-    if @messages.save
+    @group = Group.find(params[:group_id])
+    @message = ChatMessage.new(message_params)
+    if @message.save
+        # binding.pry
       redirect_to group_chat_messages_path(@group), notice: "メッセージを送信しました！"
     else
       render :index
@@ -23,7 +28,8 @@ class Public::ChatMessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:chat_message).permit(:message, :group_id)
+    params.require(:chat_message).permit(:message, :group_id).merge(end_user_id: current_end_user.id)
+    # params.require(:chat_message).permit(:message, :group_id)
   end
 
 
